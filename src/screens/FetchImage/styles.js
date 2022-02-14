e2e069800c77110486daead7a6921af24d437779
Abdulmalik Ibrahim <ibrahimmalik095@gmail.com>
2022-02-14 10:34:57 +0100

    chore: add styles file

---
+++ src/screens/FetchImage/styles.js
@@ -0,0 +1,18 @@
+import {StyleSheet} from 'react-native';
+
+export const ImageScreenStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 40,
+    paddingHorizontal: 20,
+  },
+  welcomeText: {
+    fontSize: 20,
+    fontFamily: 'Roboto-BoldItalic',
+    marginBottom: 20,
+    alignSelf: 'center',
+  },
+  dogImage: {
+    height: 400,
+  },
+});
---
+++ src/screens/Welcome/styles.js
@@ -0,0 +1,15 @@
+import {StyleSheet} from 'react-native';
+
+export const WelcomeScreenStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  welcomeText: {
+    fontSize: 20,
+    fontFamily: 'Roboto-Italic',
+    marginBottom: 20,
+  },
+});
